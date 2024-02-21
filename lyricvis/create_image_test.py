5dcf67064a1120c1b0cc4f14765c680fe4a43ab4
Christopher Mills <crmills100@yahoo.com>
2024-02-21 13:12:26 -0500

    added temp dir parameter

--- lyricvis/create_image_test.py
+++ lyricvis/create_image_test.py
@@ -19,7 +19,7 @@ class CreateImageTest(unittest.TestCase):
 
 
     def test_create_image_sd(self):
-        create_image_sd.create_image(SIZE_VGA, "'Cause then you really might know what it's like to sing the blues", True, "Produced By: Christopher Mills", FONTSIZE, IMAGE_ROOT_PATH + "A_01.png")
+        create_image_sd.create_image(SIZE_VGA, "'Cause then you really might know what it's like to sing the blues", True, "Produced By: Christopher Mills", FONTSIZE, IMAGE_ROOT_PATH + "A_01.png", IMAGE_ROOT_PATH)
         
 
 if __name__ == '__main__':
