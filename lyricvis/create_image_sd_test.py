b40c5ddacc96a20684fc9843f944bf4c4d2f91fc
Christopher Mills <crmills100@yahoo.com>
2025-01-30 17:05:40 -0500

    split out sd test

---
+++ lyricvis/create_image_sd_test.py
@@ -0,0 +1,19 @@
+import unittest
+import create_image_sd
+
+SIZE_VGA = (640, 480)
+FONTSIZE=30
+IMAGE_ROOT_PATH = "C:\\temp\\lyric_vis\\temp\\"
+
+
+class CreateImageTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_image_sd(self):
+        create_image_sd.create_image(SIZE_VGA, "'Cause then you really might know what it's like to sing the blues", True, "Produced By: Christopher Mills", FONTSIZE, IMAGE_ROOT_PATH + "A_01.png", IMAGE_ROOT_PATH)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
