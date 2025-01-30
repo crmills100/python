4628dcee0bb5dd7be2d682f32f42846bb4827869
Christopher Mills <crmills100@yahoo.com>
2025-01-30 17:14:39 -0500

    added command line options

--- mousemove/mousemove.py
+++ mousemove/mousemove.py
@@ -1,10 +1,48 @@
 import pyautogui
 import time
 import random
+import sys
+import argparse
+import pygetwindow
 
 border = 100 # border around the edge
 sleep = 30 # average sleep 
 text = " "
+
+# program options
+timeout = None # exit after timeout
+send_input = True # send logical text after mouse movement
+wait = None # time to wait at startup in minutes
+window_title = None # text to match with the active window title
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--timeout", help="exit after timeout minutes", type=int)
+parser.add_argument("--no_input", help="do not send logical text after mouse movement", action="store_true")
+parser.add_argument("--wait", help="sleep at startup for wait minutes", type=int)
+parser.add_argument("--window_title", help="only perform actions if active window title contains window_title")
+
+args = parser.parse_args()
+
+if (args.timeout):
+    timeout = args.timeout
+
+print(f"timeout: {timeout}")
+    
+if (args.no_input):
+    send_input = not args.no_input
+
+print(f"send_input: {send_input}")
+
+if (args.wait):
+    wait = args.wait
+
+print(f"wait: {wait}")
+
+if (args.window_title):
+    window_title = args.window_title
+
+print(f"window_title: {window_title}")
+
 
 def move_mouse():
     # generate random
@@ -13,6 +51,21 @@ def move_mouse():
     y = random.randint(border, max_y)
     pyautogui.moveTo(x, y, duration=1)
 
+
+def active_window_matches(window_title):
+    if (not window_title):
+        return True
+        
+    if (window_title):
+        awt = str(pygetwindow.getActiveWindowTitle())
+        if (window_title in awt):
+            print(f"'{window_title}' matches '{awt}'")
+            return True
+
+    
+    print(f"'{window_title}' does not match '{awt}'")
+
+    return False
 
 
 def send_keyboard_input(text, interval=0.1):
@@ -26,11 +79,30 @@ def send_keyboard_input(text, interval=0.1):
     time.sleep(5)  # Give some time to focus on the input field
     pyautogui.typewrite(text, interval=interval)
 
+def timedout(start_time, timeout):
 
+    if (timeout is None):
+        return False
+
+    current_time = time.time()
+    minutes_since_start = (current_time - start_time) / 60
+
+    if (minutes_since_start > timeout):
+        return True
+    
+    return False
+    
+    
+start_time = time.time()
 
 
 size = pyautogui.size();
 print(size)
+
+if (wait):
+    print(f"waiting for {wait} minutes")
+    time.sleep(wait * 60)
+
 
 max_x = size.width - (border * 2)
 max_y = size.height - (border * 2)
@@ -40,21 +112,23 @@ print(f"intial sleep for {sleep} seconds")
 time.sleep(sleep)
 
 
-while (True):
-    (x, y) = pyautogui.position()
-    print(f"x: {x} y: {y}")
+while (not timedout(start_time, timeout)):
+    if (active_window_matches(window_title)):
 
-    if (last_x == x and last_y == y):
-        # if mouse has not moved
-        move_mouse()
-        send_keyboard_input(text)
+        (x, y) = pyautogui.position()
+        print(f"x: {x} y: {y}")
 
-    last_x = x
-    last_y = y
+        if (last_x == x and last_y == y):
+            # if mouse has not moved
+            move_mouse()
+            if (send_input):
+                send_keyboard_input(text)
+
+        last_x = x
+        last_y = y
 
     s = random.randint(0, sleep * 2)
     print("sleeping for ", s, " seconds")
     time.sleep(s)
-
 
 
