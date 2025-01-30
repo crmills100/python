8d24a8ae5e0ff0205870c2848334a7b714bfd6ae
Christopher Mills <crmills100@yahoo.com>
2025-01-30 17:13:55 -0500

    split out sd test

--- lyricvis/create_image_test.py
+++ lyricvis/create_image_test.py
@@ -1,6 +1,5 @@
 import unittest
 import create_image
-import create_image_sd
 
 SIZE_VGA = (640, 480)
 FONTSIZE=30
@@ -15,11 +14,8 @@ class CreateImageTest(unittest.TestCase):
         create_image.create_image(SIZE_VGA, "This is a test!!", FONTSIZE, IMAGE_ROOT_PATH + "001.png")
 
     def test_create_blank(self):
-        create_image.create_blank(SIZE_VGA, IMAGE_ROOT_PATH + "blank.png")
+        create_image.create_blank(SIZE_VGA, IMAGE_ROOT_PATH + "blank_640x480.png")
 
-    def test_create_image_sd(self):
-        create_image_sd.create_image(SIZE_VGA, "'Cause then you really might know what it's like to sing the blues", True, "Produced By: Christopher Mills", FONTSIZE, IMAGE_ROOT_PATH + "A_01.png", IMAGE_ROOT_PATH)
-        
     def test_overlay_images(self):
         background_path = IMAGE_ROOT_PATH + "background.png"
         create_image.create_image(SIZE_VGA, "This is a test!!", FONTSIZE, background_path)
