cdaf71364275c20ee74ffb301c23d0942f0493fb
Christopher Mills <crmills100@yahoo.com>
2025-02-12 11:22:21 -0500

    number of args in function call

--- lyricvis/create_image_sd_test.py
+++ lyricvis/create_image_sd_test.py
@@ -10,8 +10,10 @@ class CreateImageTest(unittest.TestCase):
     def setUp(self):
         pass
 
+
+
     def test_create_image_sd(self):
-        create_image_sd.create_image(SIZE_VGA, "'Cause then you really might know what it's like to sing the blues", True, "Produced By: Christopher Mills", FONTSIZE, IMAGE_ROOT_PATH + "A_01.png", IMAGE_ROOT_PATH)
+        create_image_sd.create_image(SIZE_VGA, "'Cause then you really might know what it's like to sing the blues", True, "Produced By: Christopher Mills", None, FONTSIZE, IMAGE_ROOT_PATH + "A_01.png", IMAGE_ROOT_PATH)
         
 
 if __name__ == '__main__':
